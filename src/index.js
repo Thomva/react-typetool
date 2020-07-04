8cc6f8af123ef6af1529801a8b16d3769090921b
Thomva <thomvadev@gmail.com>
2020-07-04 21:08:50 +0200

    Update Typer component
    Format code, add styling and fix caret blinking.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
-import styles from './styles.module.css'
+import React, { useState, useRef, useEffect } from 'react';
 
 export const Typer = ({
   text,
@@ -9,54 +8,70 @@ export const Typer = ({
   blinkIntervalMS = 500,
   onFinish,
   showCaret = true,
-  getCaretRef
+  getCaretRef,
 }) => {
-  const [textToShow, setTextToShow] = useState('')
-  const [currentStep, setCurrentStep] = useState(0)
-  const [shouldShowCaret, setShouldShowCaret] = useState(showCaret)
-  const [finishedTyping, setFinishedTyping] = useState(false)
-  const caretRef = useRef(null)
+  const [textToShow, setTextToShow] = useState('');
+  const [currentStep, setCurrentStep] = useState(0);
+  const [shouldShowCaret, setShouldShowCaret] = useState(showCaret);
+  const [finishedTyping, setFinishedTyping] = useState(false);
+  const caretRef = useRef(null);
+
+  // The default typer style
+  const typerStyle = {
+    display: 'flex',
+    alignItems: 'flex-end',
+    whiteSpace: 'pre',
+  };
+
+  // The default caretStyle
+  const caretStyle = {
+    width: '.14rem',
+    height: '1.2rem',
+    backgroundColor: 'black',
+    marginLeft: '.14rem',
+    marginBottom: '0.1rem',
+  };
 
   // Show the caret
   const forceShowCaret = () => {
-    showCaret && setShouldShowCaret(true)
-  }
+    showCaret && setShouldShowCaret(true);
+  };
 
   // Get a reference of the caret
   useEffect(() => {
-    if (!caretRef) return
-    if (getCaretRef) getCaretRef(caretRef)
-  }, [getCaretRef, caretRef])
+    if (!caretRef) return;
+    if (getCaretRef) getCaretRef(caretRef);
+  }, [getCaretRef, caretRef]);
 
   // Update caret when property changes
   useEffect(() => {
-    setShouldShowCaret(showCaret)
-  }, [showCaret])
+    setShouldShowCaret(showCaret);
+  }, [showCaret]);
 
   useEffect(() => {
-    if (!finishedTyping) return
+    if (!finishedTyping) return;
 
     // Run the onFinish callback when typing has ended
-    if (onFinish) onFinish()
-  }, [finishedTyping])
+    if (onFinish) onFinish();
+  }, [finishedTyping]);
 
   useEffect(() => {
     // Always show the caret when the 'blink' property changes
-    forceShowCaret()
+    forceShowCaret();
 
     // Don't set blikInterval when 'blink' or 'showCaret' property is false
-    if (!blink || !showCaret) return
+    if (!blink || !showCaret) return;
 
     // Set blinkInterval
     const blinkInterval = setInterval(() => {
-      setShouldShowCaret((prev) => !prev)
-    }, blinkIntervalMS)
+      setShouldShowCaret((prev) => !prev);
+    }, blinkIntervalMS);
 
     // Clear blinkInterval on unmount
     return () => {
-      clearInterval(blinkInterval)
-    }
-  }, [blinkIntervalMS, blink, showCaret])
+      clearInterval(blinkInterval);
+    };
+  }, [blinkIntervalMS, blink, showCaret]);
 
   useEffect(() => {
     // Set the type interval
@@ -64,41 +79,51 @@ export const Typer = ({
       setCurrentStep((step) => {
         // Increase the step when it's smaller than the length of the full text
         if (step < text.length) {
-          return step + 1
+          return step + 1;
         }
 
         // Clear typeInterval when all characters are shown
-        clearInterval(typeInterval)
+        clearInterval(typeInterval);
 
         // Finish typing
-        setFinishedTyping(true)
+        setFinishedTyping(true);
 
-        return step
-      })
+        return step;
+      });
 
       // Force the caret to show
-      forceShowCaret()
-    }, stepIntervalMS)
+      forceShowCaret();
+    }, stepIntervalMS);
 
     // Clear typeInterval on unmount
     return () => {
-      clearInterval(typeInterval)
-    }
-  }, [stepIntervalMS, text])
+      clearInterval(typeInterval);
+    };
+  }, [stepIntervalMS, text]);
 
   // Update the string every step
   useEffect(() => {
-    const fullText = text
-    setTextToShow(fullText.substring(0, currentStep))
-  }, [currentStep, text])
+    const fullText = text;
+    setTextToShow(fullText.substring(0, currentStep));
+  }, [currentStep, text]);
+
+  // Make the caret blink by changing the opacity
+  useEffect(() => {
+    if (!caretRef) return;
+    caretRef.current.style.opacity = shouldShowCaret ? 1 : 0;
+  }, [shouldShowCaret]);
 
   return (
-    <div className={`typer${classes && ` ${classes}`}`}>
+    <div
+      className={`typer ${!!classes ? ` ${classes}` : ''}`}
+      style={typerStyle}
+    >
       {textToShow}
       <div
         className={`typer__caret${!shouldShowCaret && ' typer__caret--hide'}`}
         ref={caretRef}
+        style={caretStyle}
       ></div>
     </div>
-  )
-}
+  );
+};
