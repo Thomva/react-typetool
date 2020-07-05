1edab4425bc1b2cf1ee63c2db00481db614e0204
Thomva <thomvadev@gmail.com>
2020-07-05 13:20:46 +0200

    Updates

---
+++ precache-manifest.7424107b01b3dcbd55164c5022c1695e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12500336607c4f91f0b5a200ab39382b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b9f69d0061993abcf74",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "31a23f7f2f0a4b74a807",
+    "url": "/static/js/2.72ad4bcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72ad4bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9f69d0061993abcf74",
+    "url": "/static/js/main.cea3703f.chunk.js"
+  },
+  {
+    "revision": "a49f867b8ecd992fa65d",
+    "url": "/static/js/runtime-main.da2b2b7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
