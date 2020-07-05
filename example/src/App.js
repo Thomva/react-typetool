5a2a15b9b9bf491c0f81b767945d5ff70df4c422
Thomva <thomvadev@gmail.com>
2020-07-05 22:05:26 +0200

    Update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Typer } from 'react-typetool';
 
 const App = () => {
-  return <Typer text="Typertext" />
-}
+  const [startReplayA, setStartReplayA] = useState(false);
+  const [startReplayB, setStartReplayB] = useState(false);
+  const [buttonBText, setButtonBText] = useState('Start');
+
+  const replayA = () => {
+    setStartReplayA(true);
+  };
+
+  const replayB = () => {
+    setStartReplayB(true);
+    setButtonBText('Replay');
+  };
+
+
+  return (
+    <div className='app'>
+      <div className='textContainer'>
+        <Typer
+          text='Typer example'
+          onStart={() => console.log('started')}
+          onFinish={() => console.log('finished')}
+          loop={true}
+        />
+        <Typer text='Fast typing' stepIntervalMS={100} loop={true} />
+        <Typer text='Fast blinking caret' blinkIntervalMS={100} loop={true} />
+        <Typer text='No blinking caret' blink={false} loop={true} />
+        <Typer text='No caret' showCaret={false} loop={true} />
+        <div className='withButton'>
+          <Typer
+            text={`Don't loop`}
+            onStart={() => setStartReplayA(false)}
+            replay={startReplayA}
+          />
+          <button onClick={replayA}>Replay</button>
+        </div>
+        <div className='withButton'>
+          <Typer
+            text={`Don't start instantly`}
+            onStart={() => setStartReplayB(false)}
+            startInstantly={false}
+            replay={startReplayB}
+          />
+          <button onClick={replayB}>{buttonBText}</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
