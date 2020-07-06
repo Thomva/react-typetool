45e26af57052a50f8e1ea02a7e57002e00b0fcf6
Thomva <thomvadev@gmail.com>
2020-07-06 19:08:48 +0200

    Updates

---
+++ precache-manifest.47c9217dfdef0eba600ed0dab3fa90e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a6a8ad41fda7009f2eb30ad4a81d62",
+    "url": "/react-typetool/index.html"
+  },
+  {
+    "revision": "3347b58990755c51ca7a",
+    "url": "/react-typetool/static/css/main.b4846a62.chunk.css"
+  },
+  {
+    "revision": "6d88d19c2803819b620e",
+    "url": "/react-typetool/static/js/2.8661646c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-typetool/static/js/2.8661646c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3347b58990755c51ca7a",
+    "url": "/react-typetool/static/js/main.43011809.chunk.js"
+  },
+  {
+    "revision": "73eaad884ed1ffe73ac4",
+    "url": "/react-typetool/static/js/runtime-main.61d5fa9b.js"
+  }
+]);
